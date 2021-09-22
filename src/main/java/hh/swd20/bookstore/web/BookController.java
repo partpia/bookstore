248d78acde4e8231d6801c16504d60ab40a69385
partpia <partanenppia@gmail.com>
2021-09-22 13:56:57 +0300

    new functions to Controller-class and new th-templates

--- src/main/java/hh/swd20/bookstore/web/BookController.java
+++ src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hh.swd20.bookstore.domain.Book;
 import hh.swd20.bookstore.domain.BookRepository;
 
 @Controller
@@ -13,7 +17,7 @@ public class BookController {
 	@Autowired
 	private BookRepository bookRepository;
 	
-	// welcome-msg
+	// welcome-message
 	@GetMapping("/index")
 	public String helloUser() {
 		return "index";		// index.html
@@ -23,5 +27,40 @@ public class BookController {
 	public String getAllBooks(Model model) {
 		model.addAttribute("books", bookRepository.findAll());
 		return "booklist";	// booklist.html
+	}
+	
+	// create a new book
+	@GetMapping("/newbook")
+	public String createNewBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";	// addbook.html
+	}
+	
+	// save book
+	@PostMapping("/save")
+	public String saveBook(@ModelAttribute Book book) {
+		bookRepository.save(book);
+		return "redirect:/booklist";
+	}
+	
+	// delete book
+	@GetMapping("/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId) {
+		bookRepository.deleteById(bookId);
+		return "redirect:/booklist";
+	}
+	
+	// edit book
+	@GetMapping("/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", bookRepository.findById(bookId));
+		return "editbook";	// editbook.html
+	}
+	
+	// update book
+	@PostMapping("/update")
+	public String updateBook(@ModelAttribute Book book) {
+		bookRepository.save(book);
+		return "redirect:/booklist";
 	}
 }
